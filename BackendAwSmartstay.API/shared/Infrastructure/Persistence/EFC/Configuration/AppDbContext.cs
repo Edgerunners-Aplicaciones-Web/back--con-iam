4a3ab4163e6bdc101aa719fc170b868c45ec31f3
shiloox <ada44lovelace@gmail.com>
2025-11-11 19:40:44 -0500

    feat: add AppDbContext with snake case naming convention and created/updated interceptors

---
+++ BackendAwSmartstay.API/shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using BackendAwSmartstay.API.shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
+using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackendAwSmartstay.API.shared.Infrastructure.Persistence.EFC.Configuration;
+
+public class AppDbContext(DbContextOptions options) : DbContext(options)
+{
+
+    protected override void OnConfiguring(DbContextOptionsBuilder builder)
+    {
+        builder.AddCreatedUpdatedInterceptor();
+        base.OnConfiguring(builder);
+    }
+    
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        // Hotel Context
+        //builder.ApplyPublishingConfiguration();
+
+        // Profiles Context
+        //builder.ApplyProfilesConfiguration();
+        
+        // IAM Context
+        // TODO: Apply IAM configuration when available
+        // builder.ApplyIAMConfiguration();
+        
+        // General Naming Convention for the database objects
+        builder.UseSnakeCaseNamingConvention();
+    }
+}
