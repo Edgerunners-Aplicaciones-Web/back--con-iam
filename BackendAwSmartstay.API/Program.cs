023fb372ed66ababe6d84144faf00de195f5a62f
shiloox <ada44lovelace@gmail.com>
2025-11-15 14:37:10 -0500

    feat(api): add Dockerfile and environment-specific configurations for database connection handling

--- BackendAwSmartstay.API/Program.cs
+++ BackendAwSmartstay.API/Program.cs
@@ -6,13 +6,47 @@ using BackendAwSmartstay.API.Shared.Infrastructure.Interfaces.ASP.Configuration;
 using BackendAwSmartstay.API.Shared.Infrastructure.Interfaces.ASP.Configuration.Extensions;
 using BackendAwSmartstay.API.Shared.Infrastructure.Mediator.Cortex.Configuration.Extensions;
 using BackendAwSmartstay.API.shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
+using BackendAwSmartstay.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers(options => options.Conventions.Add(new KebabCaseRouteNamingConvention()));
 
-// Database Configuration
-builder.AddDatabaseConfigurationServices();
+// Add Database Connection
+if (builder.Environment.IsDevelopment())
+    builder.Services.AddDbContext<AppDbContext>(options =>
+        {
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+            if (string.IsNullOrEmpty(connectionString)) throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            options.UseMySQL(connectionString)
+                .LogTo(Console.WriteLine, LogLevel.Information)
+                .EnableSensitiveDataLogging()
+                .EnableDetailedErrors();
+        });
+else if (builder.Environment.IsProduction())
+    builder.Services.AddDbContext<AppDbContext>(options =>
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables()
+            .Build();
+
+        var connectionStringTemplate = configuration.GetConnectionString("DefaultConnection");
+        if (string.IsNullOrEmpty(connectionStringTemplate)) 
+            // Stop the application if the connection string template is not set.
+            throw new Exception("Database connection string template is not set in the configuration.");
+
+        var connectionString = Environment.ExpandEnvironmentVariables(connectionStringTemplate);
+        if (string.IsNullOrEmpty(connectionString))
+            // Stop the application if the connection string is not set.
+            throw new Exception("Database connection string is not set in the configuration.");
+
+        options.UseMySQL(connectionString)
+            .LogTo(Console.WriteLine, LogLevel.Error)
+            .EnableDetailedErrors();
+    });
 
 // OpenAPI/Swagger Configuration
 builder.AddOpenApiConfigurationServices();
@@ -31,7 +65,8 @@ var app = builder.Build();
 // Verify if the database exists and create it if it doesn't
 app.EnsureDatabaseCreated();
 
-// Configure OpenAPI/Swagger middleware
+// Configure the HTTP request pipeline.
+app.MapOpenApi();
 app.UseSwagger();
 app.UseSwaggerUI();
 
